ba720da6faa4de6c0d74cfc285fd2e0279c1c864
ajinbramantio <ajinbramantio@gmail.com>
2019-06-09 21:05:47 +0700

    create get and post

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+
+const { User } = require('../models')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', async (req, res, next) => {
+  let getAllData = await User.findAll({
+    attributes: ['id', 'fullName', 'email', 'phone']
+  }).then(data => {
+    const AllUser = JSON.stringify(data)
+    return JSON.parse(AllUser)
+  })
+  res.send({
+    message: 'success',
+    dataContacts: getAllData
+  })
+})
+router.post('/', async (req, res) => {
+  console.log(req.body)
+  const postSuccess = await User.create({
+    ...req.body
+  }).then(postResult => {
+    const result = JSON.stringify(postResult)
+    // console.log(JSON.parse(result))
 
-module.exports = router;
+    return JSON.parse(result)
+  })
+  let { createdAt, updatedAt, ...result } = postSuccess
+
+  res.send({
+    message: 'post success',
+    newData: result
+  })
+})
+
+module.exports = router
